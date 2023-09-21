252f9236d8e2a569ee533007b0e985d9848f7d1f
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-09-21 14:36:02 -0300

    Configurcoes iniciais no arquivo settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apphome',
+    'appbase',
     'bootstrap5',
     'stdimage',
 ]
