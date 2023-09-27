a198153c91546cace4e5c89d4665a7e3d4594d4a
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-09-27 18:24:30 -0300

    Melhorando metodo fale_conosco da views.py para capturar dados inseridos no formulário. Por enquanto, exibindo estes dados no console.

--- appbase/views.py
+++ appbase/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .forms import FaleConoscoForm
 
@@ -6,7 +7,22 @@ def index_home(request):
 
 
 def fale_conosco(request):
-    form = FaleConoscoForm()
+    form = FaleConoscoForm(request.POST or None)
+
+    if (str(request.method) == 'POST'):
+        if (form.is_valid()):
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print(f"\n-- MENSAGEM ENVIADA -- \nNome: {nome} \nE-mail: {email} \nAssunto: {assunto} \nMensagem: {mensagem} \n----------")
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = FaleConoscoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
+
     context = {
         'form': form
     }
