b2cc999f659696d72ba816b878c0f5521c7182a6
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-09-27 19:45:30 -0300

    forms.py classe FaleConosco com método send-mail

--- appbase/forms.py
+++ appbase/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.mail import EmailMessage
 
 
 class FaleConoscoForm(forms.Form):
@@ -6,4 +7,29 @@ class FaleConoscoForm(forms.Form):
     email = forms.CharField(label='E-mail', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=150)
     mensagem = forms.CharField(label='Mensagem', max_length=255, widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f"Nome: {nome} \nE-mail \nAssunto: {assunto} \nMensagem: {mensagem}"
+
+        mail = EmailMessage(
+            subject="E-mail enviado pelo sistema DjangoMySQL",
+            body=conteudo,
+            from_email="faleconosco@seudominio.com.br",
+            to=["contato@seudominio.com.br", "outro@seudominio.com.br"],
+            headers={"Reply-To": email}
+        )
+        mail.send()
+
+
+
+
+
+
+
+
 
